96bcec8e2ab2a381986e67cb500ec9a3e91a5487
ptomulik <pawel@tomulik.pl>
2026-04-11 15:30:47 +0000

    remove apcu_* functions from composer-require-checkers whitelist

--- tests/static-analysis/happy-path/assert-not-object-properties-identical-to.php
+++ tests/static-analysis/happy-path/assert-not-object-properties-identical-to.php
@@ -14,7 +14,7 @@ use PHPUnit\Framework\ExpectationFailedException;
 use SebastianBergmann\RecursionContext\InvalidArgumentException;
 use Tailors\PHPUnit\ObjectPropertiesIdenticalToTrait;
 
-class Assert extends \PHPUnit\Framework\Assert
+final class Assert extends \PHPUnit\Framework\Assert
 {
     use ObjectPropertiesIdenticalToTrait;
 }
