4fe6e90a735f4d4abe17599ad641a9454cea2b4c
ptomulik <pawel@tomulik.pl>
2026-04-11 16:08:07 +0000

    Merge branch '2.0' into 3.0

--- tests/static-analysis/happy-path/assert-class-properties-equal-to.php
+++ tests/static-analysis/happy-path/assert-class-properties-equal-to.php
@@ -14,7 +14,7 @@ use PHPUnit\Framework\ExpectationFailedException;
 use Tailors\PHPUnit\ClassPropertiesEqualToTrait;
 use Tailors\PHPUnit\InvalidArgumentException;
 
-class Assert extends \PHPUnit\Framework\Assert
+final class Assert extends \PHPUnit\Framework\Assert
 {
     use ClassPropertiesEqualToTrait;
 }
