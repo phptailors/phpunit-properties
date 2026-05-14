9d34849817603fa89a272435a5f6f95ffe42c1c4
ptomulik <pawel@tomulik.pl>
2026-05-14 12:39:05 +0000

    implemented RecursiveSelector stuff

--- src/Constraint/ClassPropertiesIdenticalTo.php
+++ src/Constraint/ClassPropertiesIdenticalTo.php
@@ -16,7 +16,9 @@ use Tailors\PHPUnit\Properties\ValidateExpectationsTrait;
 use Tailors\PHPUnit\Values\AbstractConstraint;
 use Tailors\PHPUnit\Values\ClassPropertySelector;
 use Tailors\PHPUnit\Values\ConstraintImplementationTrait;
+use Tailors\PHPUnit\Values\ExpectedValues;
 use Tailors\PHPUnit\Values\ValueSelectorInterface;
+use Tailors\PHPUnit\Values\ValuesInterface;
 
 /**
  * Constraint that accepts classes having properties identical to specified ones.
@@ -61,6 +63,14 @@ final class ClassPropertiesIdenticalTo extends AbstractConstraint
     protected static function makeSelector(): ValueSelectorInterface
     {
         return new ClassPropertySelector();
+    }
+
+    /**
+     * Creates instance of ValuesInterface to be used as expected values.
+     */
+    protected static function makeExpectedValues(array $array): ValuesInterface
+    {
+        return new ExpectedValues($array);
     }
 }
 
