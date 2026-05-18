542fe178ee353296f95f4ac864752186585c14b6
ptomulik <pawel@tomulik.pl>
2026-05-18 17:04:03 +0000

    Merge branch '2.1' into 3.1

--- tests/src/Constraint/ObjectPropertiesEqualToTest.php
+++ tests/src/Constraint/ObjectPropertiesEqualToTest.php
@@ -14,7 +14,7 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\Constraint\Constraint;
-use Tailors\PHPUnit\Values\ConstraintTestCase;
+use Tailors\PHPUnit\Recursive\RecursiveConstraintTestCase;
 
 /**
  * @internal This class is not covered by the backward compatibility promise
@@ -24,7 +24,7 @@ use Tailors\PHPUnit\Values\ConstraintTestCase;
 #[CoversClass(ObjectPropertiesEqualTo::class)]
 #[CoversClass(PropertiesConstraintTestCase::class)]
 #[CoversClass(ProvObjectPropertiesTrait::class)]
-#[CoversClass(ConstraintTestCase::class)]
+#[CoversClass(RecursiveConstraintTestCase::class)]
 #[Small]
 final class ObjectPropertiesEqualToTest extends PropertiesConstraintTestCase
 {
