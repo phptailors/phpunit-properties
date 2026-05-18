4048cf29ad9a01e26cc2cde6f5510d7b35c41e03
ptomulik <pawel@tomulik.pl>
2026-05-18 17:09:08 +0000

    Merge branch '5.1'

--- src/Constraint/ClassPropertiesIdenticalTo.php
+++ src/Constraint/ClassPropertiesIdenticalTo.php
@@ -14,10 +14,10 @@ use Tailors\PHPUnit\Comparator\ComparatorInterface;
 use Tailors\PHPUnit\Comparator\IdentityComparator;
 use Tailors\PHPUnit\Properties\ExpectedClassProperties;
 use Tailors\PHPUnit\Properties\ValidateExpectationsTrait;
+use Tailors\PHPUnit\Recursive\AbstractRecursiveConstraint;
+use Tailors\PHPUnit\Recursive\RecursiveConstraintSpecializationTrait;
 use Tailors\PHPUnit\Selector\ClassPropertySelector;
 use Tailors\PHPUnit\Selector\ValueSelectorInterface;
-use Tailors\PHPUnit\Values\AbstractConstraint;
-use Tailors\PHPUnit\Values\ConstraintImplementationTrait;
 use Tailors\PHPUnit\Values\ValuesInterface;
 
 /**
@@ -44,9 +44,9 @@ use Tailors\PHPUnit\Values\ValuesInterface;
  *          }
  *      }), $matcher);
  */
-final class ClassPropertiesIdenticalTo extends AbstractConstraint
+final class ClassPropertiesIdenticalTo extends AbstractRecursiveConstraint
 {
-    use ConstraintImplementationTrait;
+    use RecursiveConstraintSpecializationTrait;
     use ValidateExpectationsTrait;
 
     /**
--- tests/src/Constraint/ClassPropertiesEqualToTest.php
+++ tests/src/Constraint/ClassPropertiesEqualToTest.php
@@ -15,7 +15,7 @@ use PHPUnit\Framework\Attributes\CoversTrait;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\Constraint\Constraint;
-use Tailors\PHPUnit\Values\ConstraintTestCase;
+use Tailors\PHPUnit\Recursive\RecursiveConstraintTestCase;
 
 /**
  * @internal This class is not covered by the backward compatibility promise
@@ -25,7 +25,7 @@ use Tailors\PHPUnit\Values\ConstraintTestCase;
 #[CoversClass(ClassPropertiesEqualTo::class)]
 #[CoversClass(PropertiesConstraintTestCase::class)]
 #[CoversTrait(ProvClassPropertiesTrait::class)]
-#[CoversClass(ConstraintTestCase::class)]
+#[CoversClass(RecursiveConstraintTestCase::class)]
 #[Small]
 final class ClassPropertiesEqualToTest extends PropertiesConstraintTestCase
 {
